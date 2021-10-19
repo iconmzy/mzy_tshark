214668ee03801779f0cff4ea03ea15039209cfea
yangyang <912962140@qq.com>
2021-10-19 15:25:08 +0800

    20211019 yy 修复 tshark性能。

--- tshark.c
+++ tshark.c
@@ -954,7 +954,7 @@ int main(int argc, char *argv[]) {
                 }
 
                 /*添加注册码功能*/
-//                verify_identity_one(REGISTRATION_FILE_PATH);
+                verify_identity_one(REGISTRATION_FILE_PATH);
                 /*注册码功能结束*/
 
                 /**
@@ -2215,7 +2215,7 @@ int main(int argc, char *argv[]) {
     // tshark_debug("Aurora: do_dissection = %s", do_dissection ? "TRUE" : "FALSE");
 
     if (cf_name) {
-        //verify_identity_two(REGISTRATION_FILE_PATH);
+        verify_identity_two(REGISTRATION_FILE_PATH);
         if (EDIT_FILES_DISSECT_FLAG) {
             /*这里开始调用edit拆分大型pcap包*/
             g_print("split packet begin\n");
@@ -2347,7 +2347,7 @@ int main(int argc, char *argv[]) {
         else {
             struct stat st;
             stat(cf_name, &st);
-            //verify_identity_two(REGISTRATION_FILE_PATH);
+            verify_identity_two(REGISTRATION_FILE_PATH);
             if (S_ISDIR(st.st_mode)) {
                 /*文件夹*/
                 if (access(cf_name, R_OK) == -1) {
@@ -2360,7 +2360,7 @@ int main(int argc, char *argv[]) {
                     readFileList(cf_name, headOfDirPath);
                     pfileNameNode temp = headOfDirPath->next;
                     gboolean mutex = TRUE;
-                    //verify_identity_two(REGISTRATION_FILE_PATH);
+                    verify_identity_two(REGISTRATION_FILE_PATH);
                     while (temp != NULL) {
                         cf_name = temp->fileName_path;
                         /*将缓存的文件名全路径初始化*/
@@ -2421,7 +2421,7 @@ int main(int argc, char *argv[]) {
                 //只有一个文件
                 /*文件名*/
                 /*将缓存的文件名字初始化*/
-                //verify_identity_two(REGISTRATION_FILE_PATH);
+                verify_identity_two(REGISTRATION_FILE_PATH);
                 memset(READ_FILE_PATH, '\0', 256);
                 strcpy(READ_FILE_PATH, cf_name); //文件名含路径
                 char file_name_t[256] = {0}; //获取文件名
@@ -2685,7 +2685,7 @@ int main(int argc, char *argv[]) {
     wtap_cleanup();
     free_progdirs();
     dfilter_free(dfcode);
-    //verify_identity_two(REGISTRATION_FILE_PATH);
+    verify_identity_two(REGISTRATION_FILE_PATH);
     return exit_status;
 }
 
