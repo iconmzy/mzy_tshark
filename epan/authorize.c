14afa6bd2d692fc42e295748c79fdcae3f6050aa
yangyang <912962140@qq.com>
2021-10-19 15:53:21 +0800

    20211019 yy 修复激活显示BUG

--- epan/authorize.c
+++ epan/authorize.c
@@ -45,104 +45,181 @@ void cpu_id() {
 }
 
 ///添加注册码功能
-void verify_identity_one(const char * reg_path){
+void verify_identity_one(const char *reg_path) {
 
     char mac[256], id[256], file[256], cha[256];
-    int i, j, n, k, p, q,  sum, a, temp[256], add[256], arr[256], idx[6] = {4, 3, 5, 2, 0, 1};
+    int i, j, n, k, p, q, sum, a, temp[256], add[256], arr[256], idx[6] = {4, 3, 5, 2, 0, 1};
     FILE *fp;
 
     getMac(mac);
-    strcpy(id,globalCPUid);
+    strcpy(id, globalCPUid);
 
     strcat(id, mac);
-    n = strlen(id); j = 0;
-    for ( i = 0; i < n; i++) if (id[i] != ':' && id[i] != '-') id[j++] = id[i];
-    id[j] = 0;  n=j;
-    j = 0; for (i= 0;i<n;i+=2) id[j++] = id[i]; id[j] = 0;
+    n = strlen(id);
+    j = 0;
+    for (i = 0; i < n; i++) if (id[i] != ':' && id[i] != '-') id[j++] = id[i];
+    id[j] = 0;
+    n = j;
+    j = 0;
+    for (i = 0; i < n; i += 2) id[j++] = id[i];
+    id[j] = 0;
 
-    n = j;   k = n / 6;   p = 0;
-    for ( i = 0; i <= k; i++) {
-        for ( j = 0; j < 6; j++) { if (i * 6 + idx[j] < n) cha[p++] = id[i * 6 + idx[j]];  else  continue; }
+    n = j;
+    k = n / 6;
+    p = 0;
+    for (i = 0; i <= k; i++) {
+        for (j = 0; j < 6; j++) { if (i * 6 + idx[j] < n) cha[p++] = id[i * 6 + idx[j]]; else continue; }
     }
-    cha[p] = 0;    q = 0;
-    for(i=0;i<=k;i++){for(j=0;j<6;j++){ if (i * 6 + idx[j] < n)id[q++] = (char)(cha[i*6+idx[j]]+i); else continue;}}
+    cha[p] = 0;
+    q = 0;
+    for (i = 0; i <= k; i++) {
+        for (j = 0; j < 6; j++) {
+            if (i * 6 + idx[j] < n)
+                id[q++] = (char) (cha[i * 6 + idx[j]] + i);
+            else continue;
+        }
+    }
     id[q] = 0;
 
     printf("The machine id: %s\n", id);
     n = strlen(reg_path);
-    for(i=0;i<n;i++) file[i] = reg_path[i];file[n]=0; sprintf(file,"%sactivecode.txt",file);
-    if ((fp = fopen(file, "w")) == NULL) {  printf("openfile Err!\n");  exit(0); }  fprintf(fp, "%s", id);  fclose(fp);
+    for (i = 0; i < n; i++) file[i] = reg_path[i];
+    file[n] = 0;
+    sprintf(file, "%sactivecode.txt", file);
+    if ((fp = fopen(file, "w")) == NULL) {
+        printf("openfile Err!\n");
+        exit(0);
+    }
+    fprintf(fp, "%s", id);
+    fclose(fp);
 
     n = strlen(id);
-    for ( i = 0; i < n; i++) temp[i] = i;
-    for (i = 0; i < n; i++) { sum = 0;  for (j = 0; j < n; j++) if (j != i) sum += temp[j];   add[i] = sum;    }
-
-    for ( i = 0; i < n; i++) {
-        a = (id[i] * temp[i] + add[i]) % 126;
-        if (a >= 0 && a <= 9)  arr[i] = a;
-        else if (a >= 10 && a <= 99)  arr[i] = a % 10;
-        else { a %= 100;       if (a >= 10 && a <= 99)  arr[i] = a % 10;  else  arr[i] = a; }
+    for (i = 0; i < n; i++) temp[i] = i;
+    for (i = 0; i < n; i++) {
+        sum = 0;
+        for (j = 0; j < n; j++) if (j != i) sum += temp[j];
+        add[i] = sum;
     }
-    for ( i = 0; i < 30; i++)cha[i]=0;
-    id[0]=0;  for ( i = 0; i < n; i++) {  sprintf(cha, "%d", arr[i]);    strcat(id, cha);  }
+
+    for (i = 0; i < n; i++) {
+        a = (id[i] * temp[i] + add[i]) % 126;
+        if (a >= 0 && a <= 9) arr[i] = a;
+        else if (a >= 10 && a <= 99) arr[i] = a % 10;
+        else {
+            a %= 100;
+            if (a >= 10 && a <= 99) arr[i] = a % 10; else arr[i] = a;
+        }
+    }
+    for (i = 0; i < 30; i++)cha[i] = 0;
+    id[0] = 0;
+    for (i = 0; i < n; i++) {
+        sprintf(cha, "%d", arr[i]);
+        strcat(id, cha);
+    }
 
     n = strlen(reg_path);
-    for(i=0;i<n;i++) file[i] = reg_path[i];file[n]=0; sprintf(file,"%sregist.txt",file);
+    for (i = 0; i < n; i++) file[i] = reg_path[i];
+    file[n] = 0;
+    sprintf(file, "%sregist.txt", file);
 
-    if ((fp = fopen(file, "r")) == NULL){printf("未激活，请激活！\n"); exit(0);}
-    fread(cha,1,14,fp);   fclose(fp);
-    n=0;for(i=0;i<14;i++)n+=id[i]^cha[i];
-    if (n != 0) {printf("未激活，请激活！%s %s\n",id, cha);
-        exit(0);}
+    if ((fp = fopen(file, "r")) == NULL) {
+        printf("未激活，请激活！\n");
+        exit(0);
+    }
+    fread(cha, 1, 14, fp);
+    fclose(fp);
+    n = 0;
+    for (i = 0; i < 14; i++)n += id[i] ^ cha[i];
+    if (n != 0) {
+        printf("未激活，请激活！\n");
+        exit(0);
+    }
     printf("该设备已激活！\n");
     ///注册码功能结束
 }
 
-void verify_identity_two(const char * reg_path){
+void verify_identity_two(const char *reg_path) {
     char mac[256], id[256], file[256], cha[256];
-    int i, j, n, k, p, q,  sum, a, temp[256], add[256], arr[256], idx[6] = {4, 3, 5, 2, 0, 1};
+    int i, j, n, k, p, q, sum, a, temp[256], add[256], arr[256], idx[6] = {4, 3, 5, 2, 0, 1};
     FILE *fp;
 
     getMac(mac);
-    strcpy(id,globalCPUid);
+    strcpy(id, globalCPUid);
 
     strcat(id, mac);
-    n = strlen(id); j = 0;
-    for ( i = 0; i < n; i++) if (id[i] != ':' && id[i] != '-') id[j++] = id[i];
-    id[j] = 0;  n=j;
-    j = 0; for (i= 0;i<n;i+=2) id[j++] = id[i]; id[j] = 0;
+    n = strlen(id);
+    j = 0;
+    for (i = 0; i < n; i++) if (id[i] != ':' && id[i] != '-') id[j++] = id[i];
+    id[j] = 0;
+    n = j;
+    j = 0;
+    for (i = 0; i < n; i += 2) id[j++] = id[i];
+    id[j] = 0;
 
-    n = j;   k = n / 6;   p = 0;
-    for ( i = 0; i <= k; i++) {
-        for ( j = 0; j < 6; j++) { if (i * 6 + idx[j] < n) cha[p++] = id[i * 6 + idx[j]];  else  continue;  }
+    n = j;
+    k = n / 6;
+    p = 0;
+    for (i = 0; i <= k; i++) {
+        for (j = 0; j < 6; j++) { if (i * 6 + idx[j] < n) cha[p++] = id[i * 6 + idx[j]]; else continue; }
     }
-    cha[p] = 0;   q = 0;
-    for(i=0;i<=k;i++){for(j=0;j<6;j++){ if (i * 6 + idx[j] < n)id[q++] = (char)(cha[i*6+idx[j]]+i); else continue;}}
+    cha[p] = 0;
+    q = 0;
+    for (i = 0; i <= k; i++) {
+        for (j = 0; j < 6; j++) {
+            if (i * 6 + idx[j] < n)
+                id[q++] = (char) (cha[i * 6 + idx[j]] + i);
+            else continue;
+        }
+    }
     id[q] = 0;
 
     n = strlen(reg_path);
-    for(i=0;i<n;i++) file[i] = reg_path[i];file[n]=0; sprintf(file,"%sactivecode.txt",file);
-    if ((fp = fopen(file, "w")) == NULL) {  printf("openfile Err!\n");
-        exit(0); }  fprintf(fp, "%s", id);  fclose(fp);
+    for (i = 0; i < n; i++) file[i] = reg_path[i];
+    file[n] = 0;
+    sprintf(file, "%sactivecode.txt", file);
+    if ((fp = fopen(file, "w")) == NULL) {
+        printf("openfile Err!\n");
+        exit(0);
+    }
+    fprintf(fp, "%s", id);
+    fclose(fp);
 
     n = strlen(id);
-    for ( i = 0; i < n; i++) temp[i] = i;
-    for (i = 0; i < n; i++) { sum = 0;  for (j = 0; j < n; j++) if (j != i) sum += temp[j];   add[i] = sum;    }
-
-    for ( i = 0; i < n; i++) {
-        a = (id[i] * temp[i] + add[i]) % 126;
-        if (a >= 0 && a <= 9)  arr[i] = a;
-        else if (a >= 10 && a <= 99)  arr[i] = a % 10;
-        else { a %= 100;       if (a >= 10 && a <= 99)  arr[i] = a % 10;  else  arr[i] = a; }
+    for (i = 0; i < n; i++) temp[i] = i;
+    for (i = 0; i < n; i++) {
+        sum = 0;
+        for (j = 0; j < n; j++) if (j != i) sum += temp[j];
+        add[i] = sum;
     }
-    for ( i = 0; i < 30; i++)cha[i]=0;
-    id[0]=0;  for ( i = 0; i < n; i++) {  sprintf(cha, "%d", arr[i]);    strcat(id, cha);  }
+
+    for (i = 0; i < n; i++) {
+        a = (id[i] * temp[i] + add[i]) % 126;
+        if (a >= 0 && a <= 9) arr[i] = a;
+        else if (a >= 10 && a <= 99) arr[i] = a % 10;
+        else {
+            a %= 100;
+            if (a >= 10 && a <= 99) arr[i] = a % 10; else arr[i] = a;
+        }
+    }
+    for (i = 0; i < 30; i++)cha[i] = 0;
+    id[0] = 0;
+    for (i = 0; i < n; i++) {
+        sprintf(cha, "%d", arr[i]);
+        strcat(id, cha);
+    }
 
     n = strlen(reg_path);
-    for(i=0;i<n;i++) file[i] = reg_path[i];file[n]=0; sprintf(file,"%sregist.txt",file);
-    if ((fp = fopen(file, "r")) == NULL){printf("未激活，请激活！\n"); exit(0);}
-    fread(cha,1,14,fp);   fclose(fp);
-    n=0;for(i=0;i<14;i++)n+=id[i]^cha[i];
+    for (i = 0; i < n; i++) file[i] = reg_path[i];
+    file[n] = 0;
+    sprintf(file, "%sregist.txt", file);
+    if ((fp = fopen(file, "r")) == NULL) {
+        printf("未激活，请激活！\n");
+        exit(0);
+    }
+    fread(cha, 1, 14, fp);
+    fclose(fp);
+    n = 0;
+    for (i = 0; i < 14; i++)n += id[i] ^ cha[i];
 
     ///注册码功能结束
 }
